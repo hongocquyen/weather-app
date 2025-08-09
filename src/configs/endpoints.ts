5fb43127917da1405ad4586139f5e6b2bee2983c
Ho Ngoc Quyen <ngocquyen.work@gmail.com>
2025-08-10 03:37:33 +0700

    fix: http -> https

--- src/configs/endpoints.ts
+++ src/configs/endpoints.ts
@@ -10,8 +10,8 @@ export const WEATHER_ENDPOINT = (() => {
 
 export const GEO_ENDPOINT = (() => {
   const apis: Record<typeof ENV, string> = {
-    production: 'http://api.openweathermap.org/geo/1.0',
-    staging: 'http://api.openweathermap.org/geo/1.0',
+    production: 'https://api.openweathermap.org/geo/1.0',
+    staging: 'https://api.openweathermap.org/geo/1.0',
   };
   return apis[ENV];
 })();
